f9e55517b41a0717e494a4d2d3e44bf4a7529bea
maiger <mage_ege@hotmail.com>
2019-07-17 17:59:56 +0300

    Update recipe card style

--- src/components/RecipeListItem.vue
+++ src/components/RecipeListItem.vue
@@ -1,12 +1,12 @@
 
 <template>
-  <b-card no-body class="recipe-card">
+  <b-card no-body class="recipe-card" :class="recipeStyle">
     <b-card-img id="recipe-img" src="https://placekitten.com/360/240" alt="Resepti" top></b-card-img>
     <b-card-body>
       <b-card-title>{{recipe.title}}</b-card-title>
     </b-card-body>
     <b-card-footer>
-      <small class="text-muted">Last updated 3 mins ago</small>
+      <small class="text-muted">Tagit:</small>
     </b-card-footer>
   </b-card>
 </template>
@@ -16,16 +16,50 @@ export default {
   props: {
     recipe: Object
   },
+  data() {
+    return {
+      recipeStyle: null
+    };
+  },
+  mounted() {
+    // TODO: Do something smarter about this..
+    const rnd = Math.floor(Math.random() * 4);
+    const types = ["meat", "chicken", "fish", "vegetable"]
+    const style = {
+      meat: false,
+      chicken: false,
+      fish: false,
+      vegetable: false
+    };
+
+    style[types[rnd]] = true;
+
+    this.recipeStyle = style;
+  }
 };
 </script>
 
 <style scoped>
-  .recipe-card {
-    padding: 0;
-    border-radius: 0;
-    border: 0;
-    background-color: #90ffc4
-  }
+.recipe-card {
+  padding: 0;
+  border-radius: 0;
+  border: 0;
+}
+.meat {
+  background-color: #f78888;
+}
+
+.chicken {
+background-color: #f3d250;
+}
+
+.fish {
+  background-color: #90ccf4;
+}
+
+.vegetable {
+  background-color: #90ffc4;
+}
 
 #recipe-img {
   padding: 15px;
