61e03b00eb819ab3103d97bc7321d5b39e21bc45
maiger <mage_ege@hotmail.com>
2019-07-17 15:06:46 +0300

    Add basic components, vue-bootstrap

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <app-header></app-header>
+    This is the main dashboard
+  </div>
+</template>
+
+<script>
+import Header from "./Header"
+
+export default {
+  components: {
+    appHeader: Header
+  }
+}
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="header">
+    <b-navbar id="header-content" toggleable="lg" type="light" variant="info">
+      <b-navbar-brand href="#">Reseptikanta</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <!-- Search bar -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Hae"></b-form-input>
+          </b-nav-form>
+
+          <!-- Create a new recipe -->
+          <b-nav-item href="#">Uusi Resepti</b-nav-item>
+
+          <!-- User Options -->
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template slot="button-content">
+              <em>Käyttäjä</em>
+            </template>
+            <b-dropdown-item href="#">Profiili</b-dropdown-item>
+            <b-dropdown-item href="#">Kirjaudu ulos</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style>
+  #header-content {
+    background-color: #ececec !important
+  }
+</style>
