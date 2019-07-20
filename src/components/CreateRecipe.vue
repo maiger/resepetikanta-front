3de886f51259018295ca16a2206362eb7121e3bd
maiger <mage_ege@hotmail.com>
2019-07-20 13:40:02 +0300

    Add basic create recipe form

--- src/components/CreateRecipe.vue
+++ src/components/CreateRecipe.vue
@@ -1,16 +1,90 @@
 <template>
   <div>
     <app-header></app-header>
-    <div>Create a recipe</div>
+    <div id="new-recipe" class="container">
+      <b-form @submit="onSubmit" @reset="onReset">
+        <b-form-group id="input-group-1" label="Nimi:" label-for="input-title">
+          <b-form-input id="input-title" v-model="recipe.title" type="text" placeholder="Nimi" required></b-form-input>
+        </b-form-group>
+
+        <div class="row">
+          <div class="col-lg-8 col-sm-12">
+            <b-form-group id="input-group-2" label="Raaka-aineet:" label-for="input-ingredients">
+              <b-form-textarea id="input-ingredients" v-model="recipe.ingredients" rows="10"></b-form-textarea>
+            </b-form-group>
+          </div>
+          <div class="col-lg-4 col-sm-12">
+            <b-form-group id="input-group-3-1" label="Annoksia:" label-for="input-servings">
+              <b-form-input id="input-servings" v-model="recipe.servings" type="number" min="1"></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3-2" label="Valmistusaika:" label-for="input-preptime">
+              <b-form-input id="input-preptime" v-model="recipe.preptime" type="number" min="1"></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3-3" label="Vaikeus:" label-for="input-difficulty">
+              <b-form-input
+                id="input-difficulty"
+                v-model="recipe.difficulty"
+                type="number"
+                min="0"
+                max="5"
+              ></b-form-input>
+            </b-form-group>
+          </div>
+        </div>
+
+        <b-form-group id="input-group43" label="Valmistusohjeet:" label-for="input-instructions">
+          <b-form-textarea id="input-instructions" v-model="recipe.instructions" rows="10"></b-form-textarea>
+        </b-form-group>
+
+        <b-form-group id="input-group-5" label="Tagit:" label-for="input-tags">
+          <b-form-input id="input-tags" v-model="recipe.tags" type="text"></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Lisää</b-button>
+        <b-button type="reset" variant="danger">Tyhjennä</b-button>
+      </b-form>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from "./Header"
+import Header from "./Header";
 
 export default {
+  data() {
+    return {
+      recipe: {
+        title: null,
+        ingredients: null,
+        instructions: null,
+        difficulty: null,
+        preptime: null,
+        servings: null,
+        tags: null
+      }
+    };
+  },
   components: {
     appHeader: Header
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      console.log("New recipe:");
+      console.log(this.recipe)
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.recipe.title = null;
+      this.recipe.ingredients = null;
+      this.recipe.instructions = null;
+      this.recipe.difficulty = null;
+      this.recipe.preptime = null;
+      this.recipe.servings = null;
+      this.recipe.tags = null;
+    }
   }
 };
 </script>
--- src/components/RecipeFilter.vue
+++ src/components/RecipeFilter.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="recipe-filter" class="container">
     <b-form @reset="onReset">
-      <b-form-group id="input-group-1" label="Hakusana:" label-for="input-1">
-        <b-form-input id="input-1" v-model="filter.searchterm" type="text" placeholder="Hae"></b-form-input>
+      <b-form-group id="input-group-1" label="Hakusana:" label-for="input-searchterm">
+        <b-form-input id="input-searchterm" v-model="filter.searchterm" type="text" placeholder="Hae"></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Annoksia:" label-for="input-2">
-        <b-form-input id="input-2" v-model="filter.servings" type="number" min="1"></b-form-input>
+      <b-form-group id="input-group-2" label="Annoksia:" label-for="input-servings">
+        <b-form-input id="input-servings" v-model="filter.servings" type="number" min="1"></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="Valmistusaika:" label-for="input-3">
-        <b-form-input id="input-3" v-model="filter.preptime" type="number" min="1"></b-form-input>
+      <b-form-group id="input-group-3" label="Valmistusaika:" label-for="input-preptime">
+        <b-form-input id="input-preptime" v-model="filter.preptime" type="number" min="1"></b-form-input>
       </b-form-group>
       <b-button type="reset" variant="danger">Tyhjennä</b-button>
     </b-form>
@@ -26,7 +26,7 @@ export default {
         servings: null,
         preptime: null,
         difficulty: null,
-        tags: []
+        tags: null
       }
     };
   },
